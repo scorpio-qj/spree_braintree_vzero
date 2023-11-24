edd76c357491392bdce0929f778af922c4ae2ad7
柴奇君 <316912056@qq.com>
2023-11-24 17:46:35 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  namespace :api, defaults: { format: 'json' } do
-    namespace :v1 do
-      resource :braintree_client_token, only: :create, controller: 'braintree_client_token'
-    end
-  end
+
 end
